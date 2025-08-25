5b28ef21b98d06d53ec35b78ffd436a3a703572d
unknown <efrenpastores@gmail.com>
2025-08-26 02:10:25 +0800

    feat: Fix

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -162,12 +162,6 @@ export const LogoutIcon = ({ className }: { className?: string }) => (
     </svg>
 );
 
-export const SparklesIcon = ({ className }: { className?: string }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
-        <path d="M12 3L9.5 8.5L4 10l5.5 5.5L8 21l4-3 4 3-1.5-5.5L20 10l-5.5-1.5z"/>
-    </svg>
-);
-
 export const BellIcon = ({ className }: { className?: string }) => (
   <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
